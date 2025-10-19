bb62f85e7d8b7a1d4f7d8994ef2b5311fd4f1c31
raffernandezz <rafaelfernandesjsilvalg@gmail.com>
2025-10-18 22:58:33 -0300

    Add next.config.js to main branch for Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
+}
+
+module.exports = nextConfig
